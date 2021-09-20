8d2260dc96c352b0bcaa8ce376565e2e4e33a53b
pademss <fatma.putri@ui.ac.id>
2021-09-20 21:09:31 +0700

    PT modul2 fatma

--- fatmaJmartKD/ShipmentDuration.java
+++ fatmaJmartKD/ShipmentDuration.java
@@ -12,9 +12,9 @@ public class ShipmentDuration
     
     public static final ShipmentDuration INSTANT = new ShipmentDuration (1 << 0);
     public static final ShipmentDuration SAME_DAY = new ShipmentDuration (1 << 1);
-    public static final ShipmentDuration NEXT_DAY= new ShipmentDuration (1 << 2);
-    public static final ShipmentDuration REGULER= new ShipmentDuration (1 << 3);
-    public static final ShipmentDuration KARGO= new ShipmentDuration (1 << 4);
+    public static final ShipmentDuration NEXT_DAY = new ShipmentDuration (1 << 2);
+    public static final ShipmentDuration REGULER = new ShipmentDuration (1 << 3);
+    public static final ShipmentDuration KARGO = new ShipmentDuration (1 << 4);
     private final int bit; 
     
     private ShipmentDuration(int bit){
